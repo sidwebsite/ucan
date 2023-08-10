1a7612e8dff003378ba4b4b08c7661ac487e03f8
sid <wnagseter@gmail.com>
2023-08-10 23:04:49 +0800

    首頁新增header、navbar、news、links、download區塊

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 // bootstrap
-import * as bootstrap from 'bootstrap';
+// import * as bootstrap from 'bootstrap';
 // import { Tooltip, Toast, Popover } from 'bootstrap';
 // import Collapse from 'bootstrap/js/dist/collapse';
+// import Offcanvas from 'bootstrap/js/dist/offcanvas';
+// import Dropdown from 'bootstrap/js/dist/dropdown';
 import '../scss/style.scss';
