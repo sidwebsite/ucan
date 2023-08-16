a1a7f9bf2a18417b0323e9f87e316ec8c92bba42
sid <wnagseter@gmail.com>
2023-08-16 10:14:52 +0800

    壓縮檔案

--- dist/js/main.js
+++ dist/js/main.js
@@ -9103,6 +9103,36 @@ __webpack_require__.r(__webpack_exports__);
 
 // scss
 
+// font size
+const fontSize = document.querySelectorAll('.font-size');
+function removeActive(index1){
+    fontSize.forEach((btn2, index2) => {
+        if(index1 !== index2){
+            btn2.classList.remove("active");
+        }
+    })
+}
+fontSize.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        btn.classList.toggle("active");
+        removeActive(index);
+    });
+});
+
+const fontLarge = document.querySelector('#font-large'),
+fontNormal = document.querySelector('#font-normal'),
+fontSmall = document.querySelector('#font-small');
+
+fontLarge.addEventListener('click', () => {
+    document.body.style.fontSize = '1.15rem';
+});
+fontNormal.addEventListener('click', () => {
+    document.body.style.fontSize = '1rem';
+});
+fontSmall.addEventListener('click', () => {
+    document.body.style.fontSize = '0.875rem';
+});
+
 // print 
 
 const print = document.querySelector('#print');
