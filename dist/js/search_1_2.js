342822d45d1dcf618d03f77bf810d4dc33cc21a5
“sid” <wangsester@gmail.com>
2024-04-01 11:55:04 +0800

    最新消息完成

--- dist/js/search_1_2.js
+++ dist/js/search_1_2.js
@@ -1,17 +1,12 @@
 /******/ (() => { // webpackBootstrap
 var __webpack_exports__ = {};
-const characterTab = document.querySelectorAll('#characterTab .nav-phone');
-let triggerTabList = [].slice.call(characterTab)
-triggerTabList.forEach(function (triggerEl) {
-    let tabTrigger = new bootstrap.Tab(triggerEl)
-
-    triggerEl.addEventListener('click', function (e) {
-        e.preventDefault()
-        characterTab.forEach((nav) => {
-            nav.classList.remove('active');
-        });
-        console.log(tabTrigger);
-        tabTrigger.show()
+const characterTab = document.querySelectorAll('.nav-phone');
+characterTab.forEach(function (triggerEl) {
+    let tab = new bootstrap.Tab(triggerEl);
+    triggerEl.addEventListener('click', function (event) {
+        event.preventDefault();
+        triggerEl.classList.remove('active');
+        tab.show();
     });
 });
 /******/ })()
--- src/js/search_1_2.js
+++ src/js/search_1_2.js
@@ -1,14 +1,9 @@
-const characterTab = document.querySelectorAll('#characterTab .nav-phone');
-let triggerTabList = [].slice.call(characterTab)
-triggerTabList.forEach(function (triggerEl) {
-    let tabTrigger = new bootstrap.Tab(triggerEl)
-
-    triggerEl.addEventListener('click', function (e) {
-        e.preventDefault()
-        characterTab.forEach((nav) => {
-            nav.classList.remove('active');
-        });
-        console.log(tabTrigger);
-        tabTrigger.show()
+const characterTab = document.querySelectorAll('.nav-phone');
+characterTab.forEach(function (triggerEl) {
+    let tab = new bootstrap.Tab(triggerEl);
+    triggerEl.addEventListener('click', function (event) {
+        event.preventDefault();
+        triggerEl.classList.remove('active');
+        tab.show();
     });
 });
